fac7e3f50ad13ba903775b4954268457e627e693
Ashley Donald <ashd32@gmail.com>
2019-11-20 20:31:15 -0500

    Updates

---
+++ precache-manifest.deac542606a2ecb4617bfe8ae97f923e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b5fc87ffb6153e4370a",
+    "url": "/nappy/static/css/main.295d9637.chunk.css"
+  },
+  {
+    "revision": "4b5fc87ffb6153e4370a",
+    "url": "/nappy/static/js/main.4b5fc87f.chunk.js"
+  },
+  {
+    "revision": "417c17edd4898fee029f",
+    "url": "/nappy/static/js/runtime~main.417c17ed.js"
+  },
+  {
+    "revision": "483f7ba5428836f1822f",
+    "url": "/nappy/static/css/2.f6ab4a15.chunk.css"
+  },
+  {
+    "revision": "483f7ba5428836f1822f",
+    "url": "/nappy/static/js/2.483f7ba5.chunk.js"
+  },
+  {
+    "revision": "10360ba48b7160e65efbc33caaf36cc5",
+    "url": "/nappy/index.html"
+  }
+];
